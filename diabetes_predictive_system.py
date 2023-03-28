21e3059ca2e2481735f1875c9f33965203195664
vengat07 <128019506+vengat07@users.noreply.github.com>
2023-03-28 19:23:59 +0530

    Update diabetes_predictive_system.py

--- diabetes_predictive_system.py
+++ diabetes_predictive_system.py
@@ -18,10 +18,10 @@ input_data_as_numpy_array = np.asarray(input_data)
 # reshape the array as we are predicting for one instance
 input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
 
-prediction = loaded_model.predict(input_data_reshaped)
-print(prediction)
+rf= loaded_model.predict(input_data_reshaped)
+rf(prediction)
 
-if (prediction[0] == 0):
+if (rf[0] == 0):
   print('The person is not diabetic')
 else:
   print('The person is diabetic')
