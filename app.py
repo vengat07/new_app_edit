7e52b40fd992045a94729dd0aee4deca2725f023
vengat07 <128019506+vengat07@users.noreply.github.com>
2023-04-02 14:01:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,8 +55,9 @@ def user_report():
     report_data = pd.DataFrame(user_report_data, index=[0])
     return report_data
 
-st.sidebar.form_submit_button("Save Data")
-
+submitted= st.sidebar.form_submit_button("Save Data")
+if submitted:
+    st.success("Data Saved")
 
 # PATIENT DATA
 user_data = user_report()
