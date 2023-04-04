8c973829fc623c95da697d58ab92d6c0dd566d7e
vengat07 <128019506+vengat07@users.noreply.github.com>
2023-04-04 23:51:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -62,8 +62,11 @@ def user_report():
 
 # PATIENT DATA
 user_data = user_report()
+user_data_with_name = user_report()
+user_data_with_name.insert(0,"name",name,True)
+#df.insert(2, "Age", [21, 23, 24, 21], True)
 st.subheader('Patient Data')
-st.write(user_data)
+st.write(user_data_with_name)
 
 # MODEL
 rf  = RandomForestClassifier()
