e8c2fc7e93094100cf2780c7d79574e1a3e3eae0
vengat07 <128019506+vengat07@users.noreply.github.com>
2023-04-04 18:47:21 +0530

    Update save_csv.py

--- save_csv.py
+++ save_csv.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 user_input = st.text_input("Enter your data here:")
-df = pd.DataFrame([user_input.split(",")], columns=["Column 1", "Column 2"])
+df = pd.DataFrame([user_input.split(" ")], columns=["Column 1", "Column 2"])
 df.to_csv("example.csv", index=False)
 
 if st.button("Download CSV"):
