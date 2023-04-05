50219f13210c407d2ae56780458f0c5719a91047
vengat07 <128019506+vengat07@users.noreply.github.com>
2023-04-05 14:38:37 +0530

    Update excel.py

--- excel.py
+++ excel.py
@@ -14,28 +14,28 @@ st.sidebar.header("Options")
 
 options_form = st.sidebar.form("options_form")
 
-user_name = options_form.text_input("Name") 
+user_Name = options_form.text_input("Name") 
 
-user_age = options_form.text_input("Pregnancies") 
+user_Pregnancies = options_form.text_input("Pregnancies") 
 
-user_age = options_form.text_input("Glucose")
+user_Glucose = options_form.text_input("Glucose")
 
-user_age = options_form.text_input("BloodPressure")
+user_BloodPressure = options_form.text_input("BloodPressure")
 
-user_age = options_form.text_input("SkinThickness")
+user_SkinThickness = options_form.text_input("SkinThickness")
 
-user_age = options_form.text_input("Insulin")
+user_Insulin = options_form.text_input("Insulin")
 
-user_age = options_form.text_input("BMI")
+user_BMI = options_form.text_input("BMI")
 
-user_age = options_form.text_input("DiabetesPedigreeFunction")
+user_DiabetesPedigreeFunction = options_form.text_input("DiabetesPedigreeFunction")
 
-user_age = options_form.text_input("Age")
+user_Age = options_form.text_input("Age")
 
 add_data =  options_form.form_submit_button()
 
 if add_data:
-  new_data= {"name": user_name, "age": int(user_age)} 
+  new_data= {"Name": user_Name, "Pregnancies": int(user_Pregnancies), "Glucose": int(user_Pregnancies), "BloodPressure": int(user_BloodPressure), "SkinThickness": int(user_SkinThickness), "Insulin": int(user_Insulin), "BMI": int(user_BMI), "DiabetesPedigreeFunction": float(user_DiabetesPedigreeFunction), "Age": int(uses_Age)} 
 
   df=df.append(new_data, ignore_index=True)
 
