5b58e147f021931194701acfe97f156234e0cba1
vengat07 <128019506+vengat07@users.noreply.github.com>
2023-04-06 00:00:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -56,11 +56,11 @@ def user_report():
     return report_data
     submitted= st.sidebar.form_submit_button()
     
-data_name = pd.read_csv("data_name.csv")
+data_name = pd.read_csv("data_names.csv")
 user_data_with_name = user_report()
 if submitted:
     data_name = data_name.append(user_data_with_name, ignore_index = True)
-    data_name.to_csv("data_name.csv",index = True)
+    data_name.to_csv("data_names.csv",index = True)
     
 
 #user_data_with_name = user_report()
