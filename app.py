88a1ed72d195bed366d1d37d501ddf646fb04fab
vengat07 <128019506+vengat07@users.noreply.github.com>
2023-04-05 23:50:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -54,27 +54,17 @@ def user_report():
   }
     report_data = pd.DataFrame(user_report_data, index=[0])
     return report_data
-
-user_data_with_name = user_report()
-submitted= st.sidebar.form_submit_button()
-if submitted:
+    submitted= st.sidebar.form_submit_button()
     
-    user_report_data = {
-      'pregnancies':pregnancies,
-      'glucose':glucose,
-      'bp':bp,
-      'skinthickness':skinthickness,
-      'insulin':insulin,
-      'bmi':bmi,
-      'dpf':dpf,
-      'age':age}
-    report_data = pd.DataFrame(user_report_data, index=[0])
-    user_data_with_name = user_data_with_name.append(report_data, ignore_index=True)
-    st.success("Data Saved")
+    
 
+#user_data_with_name = user_report()
+#submitted= st.sidebar.form_submit_button()
+#if submitted:
+    
 
 # PATIENT DATA
-#user_data_with_name = user_report()
+user_data_with_name = user_report()
 #user_data_with_name = user_report()
 user_data_with_name.insert(0,"name",name,True)
 #df.insert(2, "Age", [21, 23, 24, 21], True)
