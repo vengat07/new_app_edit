5225c9efc0ca87e3ad30256d9f95157c85246f9b
vengat07 <128019506+vengat07@users.noreply.github.com>
2023-04-05 13:52:35 +0530

    Create excel.py

---
+++ excel.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+import pandas as pd
+
+st.title("Excel Update App")
+
+df = pd.read_csv("data/names.csv") 
+
+st.header("Existing File")
+
+st.write(df)
+
+st.sidebar.header("Options")
+
+options_form = st.sidebar.form("options_form")
+
+user_name options_form.text_input("Name") 
+
+user_age options_form.text_input("Age") 
+
+add_data= options_form.form_submit_button()
+
+if add_data:
+  new_data= {"name": user_name, "age": int(user_age)} 
+
+  df=df.append(new_data, ignore_index=True)
+
+  df.to_csv("data/names.csv", index-False)
