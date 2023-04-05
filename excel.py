a62f0093ef7a4d8b931c1c6dad7a31410f0f6e47
vengat07 <128019506+vengat07@users.noreply.github.com>
2023-04-05 14:06:19 +0530

    Update excel.py

--- excel.py
+++ excel.py
@@ -14,11 +14,11 @@ st.sidebar.header("Options")
 
 options_form = st.sidebar.form("options_form")
 
-user_name options_form.text_input("Name") 
+user_name = options_form.text_input("Name") 
 
-user_age options_form.text_input("Age") 
+user_age = =options_form.text_input("Age") 
 
-add_data= options_form.form_submit_button()
+add_data = options_form.form_submit_button()
 
 if add_data:
   new_data= {"name": user_name, "age": int(user_age)} 
